e0cef3d469fb3fb9a290467ad3b83226f79b39bf
AnweshDahal <anweshd2000@gmail.com>
2020-02-27 18:49:51 +0545

    First Commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+var defaultContent = `
+<div class="content" id="help">
+        INFORMATION BOARD<br>
+        <div class="title">
+            HELP
+        </div>
+        CLICK THE BUTTONS ON THE TOP OR ENTER THE FOLLOWING CODES IN THE TERMINAL TO SEE THE DIFFERENT SECTIONS<br><br>
+        qualifications : The qualifications of the creator<br><br>
+        <div class="flags">Flags: -slc, -neb11, -neb12, -ug</div><br><br>
+        skills: The skills of the creator<br><br>
+        <div class="flags">Flags: -soft, -technical</div><br><br>
+        interests: the interests of the creator<br><br>
+        <div class="flags">Flags: -style, -work</div><br><br>
+        hobbies: the hobbies of the creator<br><br>
+        clear: clear the screen<br><br>
+        help: show this screen<br><br>
+    </div>
+`;
+var qualification = `
+<div class="content" id="qualification">
+    <a href="#slc" id="lslc"></a>
+    <a href="#neb11" id="lneb11"></a>
+    <a href="#neb12" id="lneb12"></a>
+    <a href="#neb13" id="lug"></a>
+    INFORMATION BOARD<br>
+    <div class="title">
+        Qualification
+    </div>
+    Below are the Academic Qualifications of the creator.
+    <div id="slc">
+        <div class="title">
+            SLC - School Leaving Certificate Grade 10
+        </div>
+        Certification: SLC Grade 10<br>
+        Date Achieved: 2016/ 2072<br>
+        Educational Institution: Everest Higher Secondary School<br>
+        Description:<br>
+        <div class="descn">
+            SLC or School Leaving Certificate is awarded for completeing
+            Grade 10 by the National Education Board of Nepal. I studied
+            English, Nepali, Mathematics, Science, Environment and Population,
+            Optional Mathematics, Computer Science for my SLC Board.
+        </div>
+    </div>
+    <div id="neb11">
+        <div class="title">
+            NEB11 - National Examination Board Grade 11
+        </div>
+        Certification: NEB Grade 11<br>
+        Date Achieved: 2017/ 2073<br>
+        Educational Institution: Arniko Awasiya Higher Secondary School<br>
+        Description:<br>
+        <div class="descn">
+            NEB Grade 11 is the arawded on completion of Grade 11 under the 
+            HSEB or the Higher Secondary Education/ Examination Board of 
+            Neapl. I took the Science stream under which I studied 
+            Physics, Chemistry, Mathematics which included Calculus and 
+            Vector Geometry, and Computer Science.
+        </div>
+    </div>
+    <div id="neb12">
+        <div class="title">
+            NEB12 - National Examination Board Grade 12
+        </div>
+        Certification: NEB Grade 12<br>
+        Date Achieved: 2018/ 2074<br>
+        Educational Institution: Arniko Awasiya Higher Secondary School<br>
+        Description:<br>
+        <div class="descn">
+            NEB Grade 12 is the arawded on completion of Grade 12 under the
+            HSEB or the Higher Secondary Education/ Examination Board of
+            Neapl. I took the Science stream under which I studied
+            Physics, Chemistry, Mathematics which included Calculus and
+            Vector Geometry, and Nepali.
+        </div>
+    </div>
+    <div id="ug">
+        <div class="title">
+            Undergraduate
+        </div>
+        Certification: BSc. (Hons.) Computing<br>
+        Date Achieved: 2019-2022<br>
+        Educational Institution: London Metropolitan University<br>
+        Description:<br>
+        <div class="descn">
+            This is a three years Honours degree in Computing. This 
+            certification was provided by the London Metropolitan University,
+            through Islington College Nepal. Under this degree I studied:<br>
+            1. Programming using Java.<br>
+            2. Working with Relational Database Management Systems like MySQL.<br>
+            3. Working with web technologies like HTML5, CSS3, and JavaScript.<br>
+            4. Working, managing, troubleshooting a Computer's Hardware and Software components.
+        </div>
+    </div>
+</div>
+`;
